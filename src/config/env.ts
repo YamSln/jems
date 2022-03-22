70a270aa858f824aad050429136daacf9e52443a
Jamonse <yam467@gmail.com>
2022-03-22 19:46:33 +0200

    heroku testing deploy

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,3 @@
-import { config } from "dotenv";
-
-export const configEnv = (): void => {
-  if (devEnv()) {
-    config();
-  }
-};
-
 export const devEnv = (): boolean => {
   return process.env.NODE_ENV !== "prod";
 };
