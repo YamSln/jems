9bb8009637a6afe2b2e58e139ed6bcd0c0514775
Jamonse <yam467@gmail.com>
2022-12-09 23:10:01 +0200

    version update

--- src/client/src/environments/environment.prod.ts
+++ src/client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   github: 'https://github.com/YamSln/jems',
   version: '1.1.0',
-  api: 'https://jems-game.herokuapp.com',
+  api: 'https://jems.onrender.com/',
 };
