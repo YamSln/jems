f8d3ee8ecc96e789c2fa6eca7794ee6c79e8aeff
YamSln <yam467@gmail.com>
2024-02-06 12:59:12 +0200

    updates for deployment

--- src/client/src/environments/environment.prod.ts
+++ src/client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   github: 'https://github.com/YamSln/jems',
   version: '1.2.0',
-  api: 'https://jems.onrender.com',
+  api: ' https://jems-game-b66cf57bfab1.herokuapp.com',
 };
